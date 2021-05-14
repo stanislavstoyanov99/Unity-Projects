26df649f14c73356d3f2232513a4a66cd6cacb80
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-05-14 14:37:32 +0300

    Add first personal project and finish Prototype 2 game

---
+++ Create with Code/Prototype 2/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Congratulations! The ball was collected by your dog!");
+        Destroy(gameObject);
+    }
+}
---
+++ Create with Code/Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 3.0f;
+    private float randomSpawnMinInterval = 3.0f;
+    private float randomSpawnMaxInterval = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var spawnInterval = Random.Range(randomSpawnMinInterval, randomSpawnMaxInterval);
+
+        InvokeRepeating(nameof(SpawnRandomBall), startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        var ballIndex = Random.Range(0, ballPrefabs.Length);
+
+        // Generate random ball index and random spawn position
+        var spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+}
