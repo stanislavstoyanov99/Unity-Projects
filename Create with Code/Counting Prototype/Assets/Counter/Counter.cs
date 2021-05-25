7398a0e922f4bfb234dad1590203b6f7e3bb443a
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-05-25 16:31:28 +0300

    Finish with Error Project prototype
    -- Finish Counting prototype

---
+++ Create with Code/Counting Prototype/Assets/Counter/Counter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public Text CounterText;
+
+    private int counter;
+
+    private void Start()
+    {
+        counter = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        counter++;
+        CounterText.text = $"Count : {counter}";
+    }
+}
---
+++ Create with Code/Error Project/Assets/CongratScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    public TextMesh text;
+    public ParticleSystem sparksParticles;
+    
+    private List<string> textToDisplay = new List<string>();
+    private float timeToNextText;
+    private int currentTextIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToNextText = 0.0f;
+        currentTextIndex = 0;
+
+        textToDisplay.Add("Congratulation");
+        textToDisplay.Add("All Errors Fixed");
+
+        text.text = textToDisplay[0];
+        
+        sparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToNextText += Time.deltaTime;
+
+        if (timeToNextText > 1.5f)
+        {
+            timeToNextText = 0.0f;
+            
+            currentTextIndex++;
+            if (currentTextIndex >= textToDisplay.Count)
+            {
+                currentTextIndex = 0;
+            }
+
+            text.text = textToDisplay[currentTextIndex];
+        }
+    }
+}
