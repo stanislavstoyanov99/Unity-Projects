bf84bfcf712caa871f4139ba0f1d6be3d6f20e98
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-05-25 14:30:24 +0300

    Add optimization to Prototype 1 and 2
    -- SerializeField
    -- New functionality to Prototype 1 and 2

--- Create with Code/Prototype 1/Assets/Scripts/OncomingVehicleController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/OncomingVehicleController.cs
@@ -1,16 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OncomingVehicleController : MonoBehaviour
 {
     private float speed = 20f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
--- Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,26 +7,34 @@ public class PlayerController : MonoBehaviour
     public Camera hoodCamera;
     public KeyCode switchKey;
 
-    private float speed = 20f;
-    private float turnSpeed = 25f;
+    [SerializeField] private float horsePower = 0f;
+    [SerializeField] float speed;
+    [SerializeField] private const float turnSpeed = 25f;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] GameObject centerOfMass;
+
+    private Rigidbody playerRb;
     private float horizontalInput;
     private float forwardInput;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        speed = Mathf.Round(playerRb.velocity.magnitude * 3.6f); // 2.237 for mph;
+        speedometerText.SetText($"Speed: {speed} kph");
+
         // Gets player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
         // Moves the car forward based on the vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
 
         // Rotates the car based on the horizontal input
         transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
