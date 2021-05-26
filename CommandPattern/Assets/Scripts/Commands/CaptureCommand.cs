fcafdaa2113f757f95a67289fb92ec331a687fdd
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-05-26 10:54:58 +0300

    Finish CommandPattern tutorial

---
+++ CommandPattern/Assets/Scripts/Commands/CaptureCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+using static CommandManager;
+
+namespace Assets.Scripts.Commands
+{
+    public class CaptureCommand : ICommand
+    {
+        private Vector3Int m_Start;
+        private Vector3Int m_End;
+
+        private Unit m_Captured;
+        private Unit m_Capturing;
+
+        public CaptureCommand(Vector3Int start, Vector3Int end)
+        {
+            m_Start = start;
+            m_End = end;
+        }
+
+        public void Execute()
+        {
+            m_Captured = Gameboard.Instance.GetUnit(m_End);
+            m_Capturing = Gameboard.Instance.GetUnit(m_Start);
+
+            Gameboard.Instance.MoveUnit(m_Capturing, m_End);
+            Gameboard.Instance.TakeOutUnit(m_Captured);
+            Gameboard.Instance.SwitchTeam();
+        }
+
+        public void Undo()
+        {
+            Gameboard.Instance.MoveUnit(m_Capturing, m_Start);
+            Gameboard.Instance.MoveUnit(m_Captured, m_End);
+            Gameboard.Instance.SwitchTeam();
+        }
+    }
+}
---
+++ CommandPattern/Assets/Scripts/PawnUnit.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PawnUnit : Unit
+{
+    public int Distance;
+    
+    public override int GetMoveCells(Vector3Int[] result, Gameboard board)
+    {
+        int count = 0;
+        
+        for (int y = -Distance; y <= Distance; ++y)
+        {
+            for (int x = -Distance; x <= Distance; ++x)
+            {
+                if(x == 0 && y == 0)
+                    continue;
+                
+                if(Mathf.Abs(x) + Mathf.Abs(y) > Distance)
+                    continue;
+                
+                var idx = m_CurrentCell + new Vector3Int(x,0, y);
+                if (board.IsOnBoard(idx))
+                {
+                    result[count] = idx;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
