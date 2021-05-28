9ffbfa974b550ed3c200df41f531b7dd2ea88b43
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-05-28 14:37:52 +0300

    Add initial structure of custom game menu

---
+++ MyOwnGameMenu/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenOptionsMenu()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+}
